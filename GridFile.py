dc614d72a00e3c60ea51c0c36b75eff77a1ee14e
Kimberlee Finch <kmfinch42@tntech.edu>
2024-03-22 09:03:38 -0500

    added the sign up button and text for name email and password. Is currely just the bare min of what is asked if have time come back and do the extra things.

--- GridFile.py
+++ GridFile.py
@@ -5,6 +5,18 @@ from tkinter import ttk
 root = Tk()
 
 
+SignUpButton = ttk.Button(root, text= "Sign Up Now!")
+SignUpButton.grid(row=0,column=1)
+
+name = ttk.Label(root, text="Name")
+name.grid(row=1, column=0)
+
+
+email = ttk.Label(root,text= "Email")
+email.grid(row=1,column=1)
+
+password = ttk.Label(root, text="Password")
+password.grid(row=1, column=2)
 
 
 root,mainloop()
