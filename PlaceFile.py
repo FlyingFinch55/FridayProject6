12344c3b1352699465767c07bae7fd507d3f8f51
Kimberlee Finch <kmfinch42@tntech.edu>
2024-03-22 09:58:41 -0500

    added the username and password text, entry boxes for both, and the login button

--- PlaceFile.py
+++ PlaceFile.py
@@ -5,7 +5,17 @@ from tkinter import ttk
 root = Tk()
 root.title("Place")
 
+Uname = ttk.Label(root, text="Username")
+Uname.place(x=0,y=0)
+nameEntry = ttk.Entry(root)
+nameEntry.place(x=65,y=0)
 
+password = ttk.Label(root, text="Password")
+password.place(x=0,y=20)
+passEntry = ttk.Entry(root)
+passEntry.place(x=65, y=20)
 
+login = ttk.Button(root, text="Login")
+login.place(x=50,y=50)
 
 root,mainloop()
